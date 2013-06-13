95564dfa85127168c504fc2fbc2ae865119f33e5
Zack Smith <acidprime@wallcity.org>
2013-06-13 11:07:23 -0700

    Update to use subsetting
    Prior to this commit we were not using seubsettings

--- lib/puppet/provider/ipsec_secret/ini_setting.rb
+++ lib/puppet/provider/ipsec_secret/ini_setting.rb
@@ -11,12 +11,16 @@ Puppet::Type.type(:ipsec_secret).provide(
     ' '
   end
 
+
   # implement setting as the first part of the namevar
   def setting
-    username = resource[:name].split(':', 2).first
-    type     = resource[:name].split(':', 2).last
-    "#{username type}"
+    resource[:name].split(':', 2).first
   end
+
+  def subsetting
+    resource[:name].split(':', 2).last
+  end
+
   # hard code the file path (this allows purging)
   def self.file_path
     '/etc/strongswan/ipsec.secrets'
