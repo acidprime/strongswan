ea4f80f9f4be6e02f811cc30cbf63406828616ea
Zack Smith <acidprime@wallcity.org>
2013-06-13 11:31:40 -0700

    Refactor

--- lib/puppet/provider/ipsec_secret/ini_setting.rb
+++ lib/puppet/provider/ipsec_secret/ini_setting.rb
@@ -1,7 +1,7 @@
 Puppet::Type.type(:ipsec_secret).provide(
-  :ini_subsetting,
+  :ini_setting,
   # set ini_subetting as the parent provider
-  :parent => Puppet::Type.type(:ini_subsetting).provider(:ruby)
+  :parent => Puppet::Type.type(:ini_setting).provider(:ruby)
 ) do
   def section
     ''
@@ -11,17 +11,10 @@ Puppet::Type.type(:ipsec_secret).provide(
     ' '
   end
 
-  def subsetting_separator
-    ':'
-  end
-
   # implement setting as the first part of the namevar
   def setting
-    resource[:name].split(':', 2).first
-  end
-
-  def subsetting
-    resource[:name].split(':', 2).last
+    selector = resource[:name].split(':', 2).first
+    type     = resource[:name].split(':', 2).last
   end
 
   # hard code the file path (this allows purging)
