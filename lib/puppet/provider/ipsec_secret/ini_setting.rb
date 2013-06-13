7b23cfba23c926cb62255fbafb34c70a6cd2ebdd
Zack Smith <acidprime@wallcity.org>
2013-06-13 09:53:27 -0700

    Playing with formatting
    Prior to this commit we had just cut and past the inifile example

--- lib/puppet/provider/ipsec_secret/ini_setting.rb
+++ lib/puppet/provider/ipsec_secret/ini_setting.rb
@@ -3,13 +3,18 @@ Puppet::Type.type(:ipsec_secret).provide(
   # set ini_setting as the parent provider
   :parent => Puppet::Type.type(:ini_setting).provider(:ruby)
 ) do
-  # implement section as the first part of the namevar
   def section
-    resource[:name].split(':', 2).first
+    ''
   end
+
+  # implement setting as the first part of the namevar
   def setting
-    # implement setting as the second part of the namevar
-    resource[:name].split(':', 2).last
+    username = resource[:name].split(':', 2).first
+    type     = resource[:name].split(':', 2).last
+    "#{username type}"
+  end
+  def key_val_separator
+    ' '
   end
   # hard code the file path (this allows purging)
   def self.file_path
